1b26243247b6dc3805ed364538ef97d4160bd55e
Nsengiyumva7 <nsengiyumvaeugene01@gmail.com>
2023-03-17 15:02:30 +0200

    changing title

--- app/src/components/header.js
+++ app/src/components/header.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 function Header() {
-    return(
-        <nav class="bg-dark navbar-dark navbar">
-            <div className="row col-12 d-flex justify-content-center text-white">
-               <h1>APPLICATION FORM</h1>
-                
-                
-            </div>
-        </nav>
-    )
+  return (
+    <nav class="bg-dark navbar-dark navbar">
+      <div className="row col-12 d-flex justify-content-center text-white">
+        <h1>STUDENT APPLICATION FORM</h1>
+      </div>
+    </nav>
+  );
 }
 export default Header;
